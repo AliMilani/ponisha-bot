4576e92a6e468b0aad73fcb3d2d37adf62ec7392
Ali Milani <59066012+AliMilani@users.noreply.github.com>
2022-10-01 09:23:57 +0330

    remove old utils

--- scripts/currency.utils.js
+++
@@ -1,4 +0,0 @@
-export const convertRialToTomanWithSeperator = (rial) =>
-  (rial / 10).toLocaleString();
-
-console.log(convertRialToTomanWithSeperator(1000000000, { seperator: false }));
--- scripts/zip.utils.js
+++
@@ -1,3 +0,0 @@
-import extract from "extract-zip";
-
-export const unzip  = async (zipFile, dest) => await extract(zipFile, { dir: dest });
